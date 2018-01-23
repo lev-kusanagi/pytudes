21b11cc6317a162a6bfef863d692808ed33c8cbf
Lev Kusanagi <levkusanagi@gmail.com>
2018-01-23 19:10:04 +0100

    Define form_vector function.

--- py/taxicab.py
+++ py/taxicab.py
@@ -7,3 +7,18 @@ def taxicab_distance(p, q):
     p, q: np.arrays with shape 1 x n
     """
     return np.sum(np.abs(p - q))
+
+
+def form_vector(input):
+    """Return vector v, resulting from applying the sequence of inputs in
+    input
+    Returns:
+    v: two dimensional np.array
+    """
+    hdg = 'N'
+    x = 0
+    y = 0
+    for i in input:
+        hdg, x, y = compute_displacement(i, hdg)
+    return np.array([x, y])
+
